fc6ddfaf6b6bfa3ac81a142691fc548211664bfb
kermandev <46640204+kermandev@users.noreply.github.com>
2025-07-26 17:51:22 -0500

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 group = "dev.kerman"
-version = "1.1-SNAPSHOT"
+version = "1.1"
 
 repositories {
     mavenCentral()
@@ -11,6 +12,7 @@ repositories {
 
 dependencies {
     compileOnly(libs.minestom)
+    compileOnly(libs.adventure)
     testImplementation(libs.minestom.testing)
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
@@ -21,11 +23,48 @@ tasks {
         toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     }
 
+    javadoc {
+        (options as StandardJavadocDocletOptions).apply {
+            links("https://javadoc.minestom.net/")
+            links("https://jd.advntr.dev/api/${libs.versions.adventure.get()}/")
+        }
+    }
+
     test {
         useJUnitPlatform()
 
         // Required for Minestom tests to run properly
         jvmArgs("-Dminestom.viewable-packet=false")
         jvmArgs("-Dminestom.inside-test=true")
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = !version.toString().contains("SNAPSHOT"))
+
+    pom {
+        name.set("Freight")
+        description.set("A messaging wrapper for the BungeeCord protocol, designed for Minestom.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/kermandev/freight/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("kermandev")
+                name.set("Kerman")
+                url.set("https://github.com/kermandev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/kermandev/freight/")
+            connection.set("scm:git:git://github.com/kermandev/freight.git")
+            developerConnection.set("scm:git:ssh://git@github.com/kermandev/freight.git")
+        }
     }
 }
